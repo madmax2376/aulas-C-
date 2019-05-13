724fa69c0d5241f027e1b0a9d531c6071110109f
madmax2376 <mauricio@glink.net.br>
2019-05-13 18:20:51 -0300

    update

---
+++ hello-world.cs
@@ -0,0 +1,12 @@
+﻿// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class HelloWorld 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
